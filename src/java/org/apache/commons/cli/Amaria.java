c9e1f8f7210e50305d990e54592d750855e8ada8
Akash Peri <akash.peri@gmail.com>
2014-08-19 14:04:33 -0700

    A hole in the sun

--- src/java/org/apache/commons/cli/Amaria.java
+++ src/java/org/apache/commons/cli/Amaria.java
@@ -4,11 +4,7 @@ public class Amaria {
 	
 	public static void main(String[] args)
 	{
-		String s = "";
-		for(int j = 0; j < 10; j++)
-		{
-			s+=j;
-		}
+		Illumina.threnody();
 	}
 
 }
---
+++ src/java/org/apache/commons/cli/Illumina.java
@@ -0,0 +1,15 @@
+package org.apache.commons.cli;
+
+public class Illumina {
+
+	public static void threnody()
+	{
+		String s = "";
+		for(int j = 0; j < 10; j++)
+		{
+			
+			s+=j;
+		}
+	}
+	
+}
