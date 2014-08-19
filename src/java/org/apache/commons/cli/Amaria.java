54c51b7fdb669fe7cb30525a5cf6160beb0c3bcf
Akash Peri <akash.peri@gmail.com>
2014-08-19 14:00:28 -0700

    Pegboard Nerds Elizaveta - Hero

---
+++ src/java/org/apache/commons/cli/Amaria.java
@@ -0,0 +1,14 @@
+package org.apache.commons.cli;
+
+public class Amaria {
+	
+	public static void main(String[] args)
+	{
+		String s = "";
+		for(int j = 0; j < 10; j++)
+		{
+			s+=j;
+		}
+	}
+
+}
